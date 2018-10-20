32aaf8ce70ade862de91b91a856fef78bc25a083
jfc-me <fernandes_punk_@hotmail.com>
2018-10-19 20:57:27 -0400

    add Array/prox Array de Referencia

---
+++ cap_6/AHarray.java
@@ -0,0 +1,15 @@
+package cap_6;
+
+class AHarray{
+ public static void main(String[] args){
+ String aA [ ] = new String [ 3 ] ;
+ aA[ 0 ] = "LINUX";
+ aA[ 1 ] = "GNU";
+ aA[ 2 ] = "UNIX-LIKE";
+ 
+for(String p : aA){
+ System.out.println(p);
+    }
+   }
+
+}

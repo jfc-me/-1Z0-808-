a712411c58beface13bc00773f89d6eccb19ea6c
jfc-me <fernandes_punk_@hotmail.com>
2018-11-04 09:39:25 -0300

    a

---
+++ cap_7/Cont.java
@@ -0,0 +1,36 @@
+package cap_7;
+
+class Cont{
+
+ public static void main(String...run){
+ int i = 0 ;
+ while(i < 10){
+  i++;
+ if (i > 4 )
+    break;
+   System.out.println(" =:> " + i);
+            
+     }
+  
+System.out.println(" ------------------------------------");
+do { //ct 
+ i++;
+
+ System.out.println(i);
+
+}while( i < 10);
+
+
+System.out.println(" ------------------------------------");
+
+ for(int a = 0 ; a < 14 ; a++){
+   if(a > 4 )
+    continue;
+ System.out.println(a);
+                }
+System.out.println(" ------------------------------------");
+
+
+  }
+
+}
---
+++ cap_7/W3.java
@@ -0,0 +1,30 @@
+package cap_7;
+
+class W3{
+ public static void main(String...xt){
+/* do{
+  System.out.println(i++);
+ }while(i < 10);
+int i = 20;
+*/ 
+ int x = 0;
+ while(x < 10){
+
+  x++;
+ System.out.println(x);
+   }
+System.out.println("----------------------------");
+do{ 
+   System.out.println(x++);
+ // x++; 
+}while(x < 10 );
+ x++;
+// x++;
+System.out.println("-----------------------------------");
+int i = 0;
+do{
+ i++;
+ System.out.println(" =:> "+i);
+}while(i < 11);
+  }
+}
